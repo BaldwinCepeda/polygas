b4540c76383bd080e01677c6dabf93f142483af5
Baldwin <Baldwincepeda@gmail.com>
2022-08-11 12:59:36 -0400

    fix deprecated function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+
 
 function App() {
   return (
