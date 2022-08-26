5929916561403433e7933601a1ae3c02de92c3bb
Baldwin <Baldwincepeda@gmail.com>
2022-08-26 01:33:18 -0400

    added table view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
-
+import BasicTable from './components/BasicTable.js';
 function App() {
   return (
     <div className="App">
@@ -10,6 +9,7 @@ function App() {
         <h1>PolyFees</h1>
         <p>There's tons of crypto projects.
           Which ones are people actually paying to use?</p>
+        <BasicTable></BasicTable>
       </header>
 
 
---
+++ src/components/BasicTable.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+    createData('Ethereum', 0, 6.0, 24, 0),
+    createData('Polygon', 0, 9.0, 37, 0),
+    createData('Arbitrum', 0, 16.0, 24, 0),
+    createData('Avax', 0, 3.7, 67, 0),
+    createData('Evmos', 0, 16.0, 49, 0),
+];
+
+export default function BasicTable() {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name (Network)</TableCell>
+                        <TableCell align="right">1 Day Fees</TableCell>
+                        <TableCell align="right">7 Day Fees</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow
+                            key={row.name}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                            <TableCell align="right">{row.calories}</TableCell>
+
+                            <TableCell align="right">{row.protein}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
